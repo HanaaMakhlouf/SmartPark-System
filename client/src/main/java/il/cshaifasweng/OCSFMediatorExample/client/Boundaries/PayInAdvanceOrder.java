16993a350758c818e4a80f145359161bbacc7741
Marian7220 <92458438+Marian7220@users.noreply.github.com>
2023-01-07 23:47:19 +0200

    add interfaces(pay in avance order + cancael order + send complaint ) + add entites .

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Boundaries/PayInAdvanceOrder.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Boundaries/PayInAdvanceOrder.java
@@ -1,4 +1,51 @@
 package il.cshaifasweng.OCSFMediatorExample.client.Boundaries;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class PayInAdvanceOrder {
+
+    @FXML
+    private Button backBt;
+
+    @FXML
+    private TextField cardNumber;
+
+    @FXML
+    private Label cost;
+
+    @FXML
+    private TextField cvv;
+
+    @FXML
+    private MenuButton monthPayment;
+
+    @FXML
+    private TextField nameOnCard;
+
+    @FXML
+    private Label orderNumber;
+
+    @FXML
+    private Button payBt;
+
+    @FXML
+    private MenuButton yearPayment;
+
+    @FXML
+    void Pay(ActionEvent event) {
+
+    }
+
+    @FXML
+    void back(ActionEvent event)throws IOException {
+        Navigate.navigate(event , "../userBoundary.fxml");
+    }
+
 }
---
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Boundaries/SendComplaint.java
@@ -0,0 +1,30 @@
+package il.cshaifasweng.OCSFMediatorExample.client.Boundaries;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class SendComplaint {
+    @FXML
+    private Button backBt;
+
+    @FXML
+    private TextField complaintTxt;
+
+    @FXML
+    private Button sendComplaintBt;
+
+    @FXML
+    void back(ActionEvent event) throws IOException{
+        Navigate.navigate(event , "../userBoundary.fxml");
+    }
+
+    @FXML
+    void sendComplaint(ActionEvent event) {
+
+    }
+
+}
